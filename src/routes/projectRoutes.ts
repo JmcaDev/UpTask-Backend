006b5a0c07aa8b11701a21e1d460289c170fe7d7
JmcaDev <jmcavzla@gmail.com>
2024-10-19 14:52:03 -0400

    Model-Routing-Controller Task and Project, Middleware

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,50 @@
 import { Router } from "express"
+import { body, param } from "express-validator"
 import { ProjectController } from "../controllers/ProjectController"
+import { handleInputErrors } from "../middleware/validation"
+import { TaskController } from "../controllers/TaskController"
 
 const router = Router()
 
-router.post("/", ProjectController.createProject)
+//POST - Create Project
+router.post("/", 
+    body("projectName").notEmpty().withMessage("El nombre del Proyecto es obligatorio"),    
+    body("clientName").notEmpty().withMessage("El nombre del cliente es obligatorio"),    
+    body("description").notEmpty().withMessage("la descripcion del proyecto es obligatoria"),
+    handleInputErrors,
+    ProjectController.createProject
+)
+
+//GET - Get Projects
 router.get("/", ProjectController.getAllProjects)
+
+//GET - Get project by id
+router.get("/:id", 
+    param("id").isMongoId().withMessage("ID no valido"),
+    handleInputErrors,
+    ProjectController.getProjectById
+)
+
+//PUT - Update Project
+router.put("/:id",
+    param("id").isMongoId().withMessage("ID no valido"),
+    body("projectName").notEmpty().withMessage("El nombre del Proyecto es obligatorio"),    
+    body("clientName").notEmpty().withMessage("El nombre del cliente es obligatorio"),    
+    body("description").notEmpty().withMessage("la descripcion del proyecto es obligatoria"),
+    handleInputErrors,
+    ProjectController.updateProject
+)
+
+//DELETE - Delete a project
+router.delete("/:id", 
+    param("id").isMongoId().withMessage("ID no valido"),
+    handleInputErrors,
+    ProjectController.deleteProject
+)
+
+//Routes for task
+router.post("/:projectId/task",
+    TaskController.createTask
+)
 
 export default router
