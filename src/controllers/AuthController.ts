adfe157029cd8b4196d62902db365ff96723b55b
JmcaDev <jmcavzla@gmail.com>
2024-11-16 12:33:32 -0400

    Module Token and Email

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,9 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import Token from "../models/Token"
+import { generateToken } from "../utils/token"
+import { AuthEmail } from "../emails/AuthEmail"
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
@@ -21,9 +24,45 @@ export class AuthController {
 
             //Hash password
             user.password = await hashPassword(password)
-            await user.save()
+
+            //Generar Token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+
+            //Enviar email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+
+            await Promise.allSettled([user.save(), token.save()])
 
             res.send("Cuenta creada, revisa tu email para confirmarla")
+        } catch (error) {
+            res.status(500).json({error: "Hubo un error"})
+        }
+    }
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        try {
+            const {token} = req.body
+
+            const tokenExist = await Token.findOne({token})
+
+            if(!tokenExist){
+                const error = new Error("Token no valido")
+                res.status(401).json({error: error.message})
+                return
+            }
+
+            const user = await User.findById(tokenExist.user)
+            user.confirmed = true
+
+            await Promise.allSettled([user.save(),tokenExist.deleteOne()])
+            res.send("Cuenta confirmada correctamente")
+
         } catch (error) {
             res.status(500).json({error: "Hubo un error"})
         }
