2ee9e18ceeccafb3668e34622f13309eee8debd7
JmcaDev <jmcavzla@gmail.com>
2024-11-25 20:49:01 -0400

    Restore password

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { AuthController } from "../controllers/AuthController"
-import { body } from "express-validator"
+import { body, param } from "express-validator"
 import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
@@ -43,6 +43,34 @@ router.post("/request-code",
         .notEmpty().withMessage("El email no puede ir vacio").isEmail().withMessage("Email no valido"),
     handleInputErrors,
     AuthController.requestConfirmationCode
+)
+
+router.post("/forgot-password",
+    body("email")
+        .notEmpty().withMessage("El email no puede ir vacio").isEmail().withMessage("Email no valido"),
+    handleInputErrors,
+    AuthController.forgotPassword
+)
+
+router.post("/validate-token",
+    body("token")
+        .notEmpty().withMessage("El token no puede ir vacio"),
+    handleInputErrors,
+    AuthController.validateToken
+)
+
+router.post("/update-password/:token",
+    param("token").isNumeric().withMessage("Token no valido"),
+    body("password")
+        .notEmpty().withMessage("El password no puede ir vacio").isLength({min: 8}).withMessage("El password debe tener minimo 8 caracteres"),
+    body("password_confirmation").custom((value, {req}) => {
+       if(value !== req.body.password){
+        throw new Error("Los password no son iguales")
+       }
+       return true
+    }),
+    handleInputErrors,
+    AuthController.updatePasswordWithToken
 )
 
 export default router
